a89c321d3765f836058add41e849097970cf9103
Justin DiEmmanuele <justindiemmanuele@Justins-MacBook-Air.local>
2021-10-12 15:04:44 -0400

    getters and setters

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,16 @@
 import java.util.ArrayList;
 
 public abstract class Player {
+    static int totalPlayers = 0;
+
     String name;
     int numWins;
     ArrayList<Hand> hand;
     boolean dealer;
+
+    public Player() {
+        this(String.format("Player %S", totalPlayers));
+    }
 
     public Player(String name) {
         this(name, false);
@@ -15,6 +21,34 @@ public abstract class Player {
         numWins = 0;
         hand = new ArrayList<Hand>();
         hand.add(new Hand());
+        this.dealer = dealer;
+    }
+
+    public static int getTotalPlayers() {
+        return totalPlayers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumWins() {
+        return numWins;
+    }
+
+    public void setNumWins(int numWins) {
+        this.numWins = numWins;
+    }
+
+    public boolean isDealer() {
+        return dealer;
+    }
+
+    public void setDealer(boolean dealer) {
         this.dealer = dealer;
     }
 
