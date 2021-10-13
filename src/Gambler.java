744cc41106c3e0f8db5840907c4976f47c4098e3
shilpen12 <shilpen12@gmail.com>
2021-10-12 21:19:04 -0400

    Adding TriantaEnaPlayer class and updating Gambler interface.

--- src/Gambler.java
+++ src/Gambler.java
@@ -1,13 +1,14 @@
+//Gambler interface used for force implementation of methods needed for betting card games
 interface Gambler{
 
   //Method to place a bet to be used by a player
-  void placeBet(int bet);
+  void placeBet(double bet);
 
   //Method to get a bet to be placed by a player
-  int getBet();
+  double getBet();
 
   //Method to set the players balance
-  void setBalance();
+  void setBalance(double balance);
 
   //Method to end a turn of a player's hand
   void endHand();
